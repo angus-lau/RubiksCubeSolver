628bb391572e727cd8d989ca5bf724765df5f4b2
angus-lau <anguslau91@gmail.com>
2024-09-06 17:53:26 -0700

    update color on faces and created ability to cycle through faces

--- video_capture.py
+++ video_capture.py
@@ -12,22 +12,18 @@ if not cap.isOpened():
     print("Cannot open camera")
     exit()
 
-# Determine ROI sizes
+# Define ROI sizes
 roi_width = 200
 roi_height = 200
 
-# Determine cell size
+# Define cell sizes
 num_columns = 3
 num_rows = 3
 cell_width = roi_width // num_columns
 cell_height = roi_height // num_rows
 
-# Define face map
-face_map = {
-    (0,0): 'F', (0,1): 'F', (0,2): 'F',
-    (1,0): 'F', (1,1): 'F', (1,2): 'F',
-    (2,0): 'F', (2,1): 'F', (2,2): 'F',
-}
+# Define faces
+faces = ['F', 'T', 'L', 'R', 'B', 'D']
 
 detected_colors = {face: [['Unknown' for _ in range(num_columns)] for _ in range(num_rows)] for face in rubiks_cube.faces}
 
@@ -36,13 +32,11 @@ color_ranges = {
     'Blue': (np.array([100, 80, 0]), np.array([140, 255, 255])),
     'Green': (np.array([40, 70, 70]), np.array([80, 255, 255])),
     'Orange': (np.array([0, 70, 50]), np.array([10, 255, 255])),
-    'Red': (np.array([0, 50, 50]), np.array([10, 255, 255])),
-    'Red': (np.array([170, 50, 50]), np.array([180, 255, 255])),
+    'Red1': (np.array([0, 50, 50]), np.array([10, 255, 255])),
+    'Red2': (np.array([170, 50, 50]), np.array([180, 255, 255])),
     'Yellow': (np.array([25, 80, 200]), np.array([40, 255, 255])),
     'White': (np.array([30, 0, 150]), np.array([90, 20, 255]))
 }
-
-selected_face = input("Enter the face to update (F, B, L, R, T, D): ").upper()
 
 while True:
     # Capture frame-by-frame
@@ -53,104 +47,123 @@ while True:
         print("Cannot receive frame. Exiting...")
         break
 
-    frame_height, frame_width = frame.shape[:2]
+    # Check if there are any remaining faces to scan
+    if len(faces) == 0:
+        break
 
-    # Calculate position for ROI
-    x_center = frame_width // 2
-    y_center = frame_height // 2
+    # Go through list of faces to check
+    while len(faces) > 0:
+        curr_face = faces.pop(0)
+        print(f"Scanning {curr_face}")
 
-    # Find corners for ROI
-    x1 = x_center - roi_width // 2
-    y1 = y_center - roi_height // 2
-    x2 = x_center + roi_width // 2
-    y2 = y_center + roi_height // 2
+        # Capture frame
+        while True: 
+            print("Press C to capture frame.")
+            if cv.waitKey(1) == ord('c'):
+                print("Processing frame.")
+                frame_height, frame_width = frame.shape[:2]
 
-    # Create ROI
-    roi = frame[y1:y2, x1:x2]
+                # Calculate position for ROI
+                x_center = frame_width // 2
+                y_center = frame_height // 2
 
-    # Convert frame to HSV
-    hsv_roi = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
+                # Find corners for ROI
+                x1 = x_center - roi_width // 2
+                y1 = y_center - roi_height // 2
+                x2 = x_center + roi_width // 2
+                y2 = y_center + roi_height // 2
 
-    # Create mask of 0s for ROI
-    combined_roi_mask = np.zeros((roi_height, roi_width), dtype=np.uint8)
+                # Create ROI
+                roi = frame[y1:y2, x1:x2]
 
-    # Loop through each cell in the 3x3 grid
-    for row in range(num_rows):
-        for col in range(num_columns):
+                # Convert frame to HSV
+                hsv_roi = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
 
-            # Define cell boundary
-            cell_x1 = col * cell_width
-            cell_y1 = row * cell_height
-            cell_x2 = (col + 1) * cell_width
-            cell_y2 = (row + 1) * cell_height
+                # Create mask of 0s for ROI
+                combined_roi_mask = np.zeros((roi_height, roi_width), dtype=np.uint8)
 
-            # Extract cell from ROI
-            cell = roi[cell_y1:cell_y2, cell_x1:cell_x2]
-            hsv_cell = hsv_roi[cell_y1:cell_y2, cell_x1:cell_x2]
+                # Loop through each cell in the 3x3 grid
+                for row in range(num_rows):
+                    for col in range(num_columns):
 
-            # Initialize masks
-            combined_cell_mask = np.zeros(hsv_cell.shape[:2], dtype=np.uint8)
+                        # Define cell boundary
+                        cell_x1 = col * cell_width
+                        cell_y1 = row * cell_height
+                        cell_x2 = (col + 1) * cell_width
+                        cell_y2 = (row + 1) * cell_height
 
-            # Create masks for each color
-            for color, (lower, upper) in color_ranges.items():
-                mask = cv.inRange(hsv_cell, lower, upper)
-                combined_cell_mask = cv.bitwise_or(combined_cell_mask, mask)
+                        # Extract cell from ROI
+                        cell = roi[cell_y1:cell_y2, cell_x1:cell_x2]
+                        hsv_cell = hsv_roi[cell_y1:cell_y2, cell_x1:cell_x2]
 
-                if np.any(mask):
-                    detected_color = color
-                detected_colors[row][col] = detected_color
+                        # Initialize masks
+                        combined_cell_mask = np.zeros(hsv_cell.shape[:2], dtype=np.uint8)
 
-            # Update face in Rubiks Cube
-            if selected_face in rubiks_cube.faces:
-                rubiks_cube.update_face(selected_face, detected_colors)
-            else:
-                print("Invalid Face")
-
-            # Display cube
-            rubiks_cube.displayCube()
-
-            # Update the combined mask for the ROI
-            combined_roi_mask[cell_y1:cell_y2, cell_x1:cell_x2] = combined_cell_mask
-
-            # Find contours in the cell
-            cnts = cv.findContours(combined_cell_mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-            cnts = imutils.grab_contours(cnts)
-
-            # Draw contours and add text
-            for c in cnts:
-                area = cv.contourArea(c)
-                if area > 500:
-                    cv.drawContours(roi, [c + np.array([cell_x1, cell_y1])], -1, (0, 255, 0), 2)
-                    M = cv.moments(c)
-                    if M["m00"] != 0:
-
-                        # Calculate centroid of contour
-                        cx = int(M["m10"] / M["m00"])
-                        cy = int(M["m01"] / M["m00"])
-
-                        # Determine color
-                        color_detected = "Unknown"
+                        # Create masks for each color
                         for color, (lower, upper) in color_ranges.items():
-                            if cv.inRange(np.array([[hsv_cell[cy, cx]]]), lower, upper).any():
-                                color_detected = color
-                                break
+                            mask = cv.inRange(hsv_cell, lower, upper)
+                            combined_cell_mask = cv.bitwise_or(combined_cell_mask, mask)
 
-                        # Draw centroid and text
-                        cv.circle(roi, (cx + cell_x1, cy + cell_y1), 7, (255, 255, 255), -1)
-                        cv.putText(roi, color_detected, (cx + cell_x1 - 20, cy + cell_y1 - 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                            if np.any(mask):
+                                if color in ['Red1', 'Red2']:
+                                     detected_colors[row][col] = 'Red'
+                                else:
+                                    detected_color = color
+                                
+                            detected_colors[row][col] = detected_color
 
-    # Apply the combined mask to the ROI
-    result_roi = cv.bitwise_and(roi, roi, mask=combined_roi_mask)
+                        # Update the combined mask for the ROI
+                        combined_roi_mask[cell_y1:cell_y2, cell_x1:cell_x2] = combined_cell_mask
 
-    # Update the frame with the processed ROI
-    frame[y1:y2, x1:x2] = roi
+                        # Find contours in the cell
+                        cnts = cv.findContours(combined_cell_mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+                        cnts = imutils.grab_contours(cnts)
 
-    # Draw rectangle around ROI
-    cv.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                        # Draw contours and add text
+                        for c in cnts:
+                            area = cv.contourArea(c)
+                            if area > 500:
+                                cv.drawContours(roi, [c + np.array([cell_x1, cell_y1])], -1, (0, 255, 0), 2)
+                                M = cv.moments(c)
+                                if M["m00"] != 0:
 
+                                    # Calculate centroid of contour
+                                    cx = int(M["m10"] / M["m00"])
+                                    cy = int(M["m01"] / M["m00"])
+
+                                    # Determine color
+                                    color_detected = "Unknown"
+                                    for color, (lower, upper) in color_ranges.items():
+                                        if cv.inRange(np.array([[hsv_cell[cy, cx]]]), lower, upper).any():
+                                            color_detected = color
+                                            break
+
+                                    # Draw centroid and text
+                                    cv.circle(roi, (cx + cell_x1, cy + cell_y1), 7, (255, 255, 255), -1)
+                                    cv.putText(roi, color_detected, (cx + cell_x1 - 20, cy + cell_y1 - 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+                rubiks_cube.update_face(curr_face, detected_colors)
+
+                # Apply the combined mask to the ROI
+                result_roi = cv.bitwise_and(roi, roi, mask=combined_roi_mask)
+
+                # Update the frame with the processed ROI
+                frame[y1:y2, x1:x2] = roi
+
+                # Draw rectangle around ROI
+                cv.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+            # Display current face of rubiks cube
+            rubiks_cube.displayCube(curr_face)
+            print("Is this correct? Y/N")
+            if cv.waitKey(1) == ord("Y"):
+                break
+            
     # Display the video
     cv.imshow("Live", frame)
     cv.imshow("Masked", result_roi)
+
+    # Exit
     if cv.waitKey(1) == ord('q'):
         break
 
