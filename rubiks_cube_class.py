65c943bc8970c1caf9b0eaf58e9b00087b3d888d
angus-lau <anguslau91@gmail.com>
2024-09-06 17:41:17 -0700

    change displayCube() to display only one side at a time

--- rubiks_cube_class.py
+++ rubiks_cube_class.py
@@ -6,18 +6,14 @@ class RubiksCube:
             'L': [['G', 'G', 'G'], ['G', 'G', 'G'], ['G', 'G', 'G']],
             'R': [['B', 'B', 'B'], ['B', 'B', 'B'], ['B', 'B', 'B']],
             'T': [['W', 'W', 'W'], ['W', 'W', 'W'], ['W', 'W', 'W']],
-            'B': [['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O']]
+            'D': [['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O']]
         }
 
-    #update colors on specific face
+    # Update colors on specific face
     def update_face(self, face, colors):
         self.faces[face] = colors
 
-    #display cube
-    def displayCube(self):
-        print("Current state of the Rubik's Cube:")
-        for face, colors in self.faces.items():
-            print(f"{face}:")
-            for row in colors:
-                print(row)
-            print()
+    # Display cube
+    def displayCube(self, face):
+        print(f"Current state of {face}.")
+        print(self.faces[face])
