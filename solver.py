e1426ad5acd7f927f8f5937f1135305c1dff63c8
Angus Lau <anguslau91@gmail.com>
2024-10-11 01:59:02 -0700

    add TODOs, worked on rotating face

---
+++ solver.py
@@ -0,0 +1,10 @@
+#TODO rotate face clockwise
+#TODO rotate face counter clockwise
+#TODO rotate top row clockwise
+#TODO rotate top row counter clockwise
+#TODO rotate bottom row clockwise
+#TODO rotate bottom row counter clockwise
+#TODO rotate right column clockwise
+#TODO rotate right column counter clockwise 
+#TODO rotate left column clockwise
+#TODO rotate left column counter clockwise
