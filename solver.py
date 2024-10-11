4f4dee20eb5415323f5b5b0598b484e9bfef5730
Angus Lau <anguslau91@gmail.com>
2024-10-11 02:00:22 -0700

    add TODOs, worked on rotating faces

--- solver.py
+++ solver.py
@@ -1,5 +1,30 @@
-#TODO rotate face clockwise
-#TODO rotate face counter clockwise
+import rubiks_cube_class as rc
+import checker
+
+#TODO adjust neighbour faces as well
+# Rotate face clockwise
+def rotate_face_clockwise(face, cube):
+    # Get current cube state
+    curr_cube = rc.get_faces()
+    # Get colors of the face
+    curr_face = rc.get_faces()[face]
+    # rotate clockwise
+    rotated_face = list(zip(*curr_face[::-1]))
+    # Update face colors
+    cube.update_face(face, rotated_face)
+
+#TODO adjust neighbour faces as well
+# Rotate face counter clockwise
+def rotate_face_counter_clockwise(face, cube):
+    # Get current cube state
+    curr_cube = rc.get_faces()
+    # Get colors of the face
+    curr_face = rc.get_faces()[face]
+    # rotate counter clockwise
+    rotated_face = list(reversed(list(zip(*curr_face))))
+    # Update face colors
+    cube.update_face(face, rotated_face)
+
 #TODO rotate top row clockwise
 #TODO rotate top row counter clockwise
 #TODO rotate bottom row clockwise
