a2b3793a5c3d06f9eb9ad2577d592b61910a7d30
Angus Lau <anguslau91@gmail.com>
2024-10-14 02:21:54 -0700

    work on tests

--- test_checker.py
+++ test_checker.py
@@ -2,79 +2,72 @@ import unittest
 import checker
 
 class TestChecker(unittest.TestCase):
-    # TODO !!!
     def test_white_cross_check(self):
         # No white cross
         no_white_cross = {
-            'F': [['R', 'B', 'R'], 
-                  ['O', 'G', 'Y'], 
-                  ['Y', 'G', 'O']],
-            'B': [['O', 'Y', 'R'], 
-                  ['Y', 'B', 'R'], 
-                  ['R', 'W', 'Y']],
-            'L': [['B', 'G', 'B'], 
-                  ['B', 'O', 'Y'], 
-                  ['O', 'O', 'O']],
-            'R': [['G', 'G', 'B'], 
-                  ['G', 'R', 'B'],
-                  ['W', 'O', 'W']],
-            'T': [['W', 'R', 'W'], 
-                  ['O', 'W', 'R'], 
-                  ['Y', 'W', 'Y']],
-            'D': [['B', 'W', 'G'], 
-                  ['B', 'Y', 'W'], 
-                  ['G', 'R', 'G']]
+            'F': [['R', 'B', 'R'], ['O', 'G', 'Y'], ['Y', 'G', 'O']],
+            'B': [['O', 'Y', 'R'], ['Y', 'B', 'R'], ['R', 'W', 'Y']],
+            'L': [['B', 'G', 'B'], ['B', 'O', 'Y'], ['O', 'O', 'O']],
+            'R': [['G', 'G', 'B'], ['G', 'R', 'B'], ['W', 'O', 'W']],
+            'T': [['W', 'R', 'W'], ['O', 'W', 'R'], ['Y', 'W', 'Y']],
+            'D': [['B', 'W', 'G'], ['B', 'Y', 'W'], ['G', 'R', 'G']]
         }
         # White cross with correct edge pieces
         white_cross_correct_edge = {
-            'F': [['W', 'G', 'Y'], 
-                  ['Y', 'G', 'O'], 
-                  ['O', 'G', 'B']],
-            'B': [['R', 'B', 'Y'], 
-                  ['Y', 'B', 'R'], 
-                  ['B', 'G', 'Y']],
-            'L': [['G', 'O', 'R'], 
-                  ['G', 'O', 'R'], 
-                  ['G', 'B', 'G']],
-            'R': [['O', 'R', 'B'], 
-                  ['Y', 'R', 'B'],
-                  ['R', 'O', 'O']],
-            'T': [['O', 'W', 'Y'], 
-                  ['W', 'W', 'W'], 
-                  ['G', 'W', 'B']],
-            'D': [['W', 'O', 'W'], 
-                  ['R', 'Y', 'B'], 
-                  ['R', 'Y', 'W']]
+            'F': [['W', 'G', 'Y'], ['Y', 'G', 'O'], ['O', 'G', 'B']],
+            'B': [['R', 'B', 'Y'], ['Y', 'B', 'R'], ['B', 'G', 'Y']],
+            'L': [['G', 'O', 'R'], ['G', 'O', 'R'], ['G', 'B', 'G']],
+            'R': [['O', 'R', 'B'], ['Y', 'R', 'B'], ['R', 'O', 'O']],
+            'T': [['O', 'W', 'Y'], ['W', 'W', 'W'], ['G', 'W', 'B']],
+            'D': [['W', 'O', 'W'], ['R', 'Y', 'B'], ['R', 'Y', 'W']]
         }
         # White cross with incorrect edge pieces
         white_cross_incorrect_edge = {
-            'F': [['W', 'R', 'Y'], 
-                  ['Y', 'G', 'O'], 
-                  ['O', 'G', 'B']],
-            'B': [['R', 'O', 'Y'], 
-                  ['Y', 'B', 'R'], 
-                  ['B', 'W', 'Y']],
-            'L': [['G', 'G', 'R'], 
-                  ['G', 'O', 'R'], 
-                  ['G', 'B', 'G']],
-            'R': [['O', 'B', 'B'], 
-                  ['Y', 'R', 'B'],
-                  ['R', 'O', 'O']],
-            'T': [['O', 'W', 'Y'], 
-                  ['W', 'W', 'W'], 
-                  ['G', 'W', 'B']],
-            'D': [['W', 'O', 'W'], 
-                  ['R', 'Y', 'B'], 
-                  ['R', 'Y', 'W']]
+            'F': [['W', 'R', 'Y'], ['Y', 'G', 'O'], ['O', 'G', 'B']],
+            'B': [['R', 'O', 'Y'], ['Y', 'B', 'R'], ['B', 'W', 'Y']],
+            'L': [['G', 'G', 'R'], ['G', 'O', 'R'], ['G', 'B', 'G']],
+            'R': [['O', 'B', 'B'], ['Y', 'R', 'B'], ['R', 'O', 'O']],
+            'T': [['O', 'W', 'Y'], ['W', 'W', 'W'], ['G', 'W', 'B']],
+            'D': [['W', 'O', 'W'], ['R', 'Y', 'B'], ['R', 'Y', 'W']]
         }
         self.assertTrue(checker.white_cross_check(white_cross_correct_edge))
         self.assertFalse(checker.white_cross_check(no_white_cross)) 
         self.assertFalse(checker.white_cross_check(white_cross_incorrect_edge))
-        pass
 
-    # TODO !!!
     def test_white_corners_check(self):
-        pass
+        # White corners with correct corner pieces
+        white_corners_correct= {
+            'F': [['G', 'G', 'G'], ['B', 'G', 'G'], ['Y', 'Y', 'O']],
+            'B': [['B', 'B', 'B'], ['R', 'B', 'G'], ['B', 'B', 'Y']],
+            'L': [['O', 'O', 'O'], ['O', 'O', 'O'], ['B', 'B', 'R']],
+            'R': [['R', 'R', 'R'], ['Y', 'R', 'G'], ['G', 'O', 'R']],
+            'T': [['W', 'W', 'W'], ['W', 'W', 'W'], ['W', 'W', 'W']],
+            'D': [['G', 'R', 'Y'], ['R', 'Y', 'Y'], ['O', 'Y', 'Y']]
+        }
+
+        # White corners with incorrect corner pieces
+        white_corners_false = {
+            'F': [['W', 'G', 'Y'], ['Y', 'G', 'O'], ['O', 'G', 'B']],
+            'B': [['R', 'B', 'Y'], ['Y', 'B', 'R'], ['B', 'G', 'Y']],
+            'L': [['G', 'O', 'R'], ['G', 'O', 'R'], ['G', 'B', 'G']],
+            'R': [['O', 'R', 'B'], ['Y', 'R', 'B'], ['R', 'O', 'O']],
+            'T': [['O', 'W', 'Y'], ['W', 'W', 'W'], ['G', 'W', 'B']],
+            'D': [['W', 'O', 'W'], ['R', 'Y', 'B'], ['R', 'Y', 'W']]
+        }
+
+        # White corners with wrong corner pieces
+        white_corners_wrong_corner = {
+            'F': [['R', 'G', 'O'], ['Y', 'G', 'Y'], ['G', 'B', 'Y']],
+            'B': [['O', 'B', 'R'], ['Y', 'B', 'G'], ['Y', 'B', 'G']],
+            'L': [['B', 'O', 'G'], ['R', 'O', 'G'], ['R', 'O', 'O']],
+            'R': [['G', 'R', 'B'], ['R', 'R', 'O'], ['B', 'B', 'B']],
+            'T': [['W', 'W', 'W'], ['W', 'W', 'W'], ['W', 'W', 'W']],
+            'D': [['Y', 'Y', 'O'], ['G', 'Y', 'R'], ['Y', 'O', 'R']]
+        }
+
+        self.assertTrue(checker.white_corners_check(white_corners_correct))
+        self.assertFalse(checker.white_corners_check(white_corners_false))
+        self.assertFalse(checker.white_corners_check(white_corners_wrong_corner))
 
     # TODO !!!
     def test_middle_edge(self):
