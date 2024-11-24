6f3f556b26865dc5e6deaff85ec7661bba8f3269
Angus Lau <anguslau91@gmail.com>
2024-11-24 15:09:29 -0800

    work on solving white cross

--- solver.py
+++ solver.py
@@ -1,277 +1,142 @@
-import rubiks_cube_class as rc
-import checker
-
-def rotate_face_clockwise(face, cube):
-    # Rotate array clockwise
-    # Rotate array clockwise
-    # rotated_face = list(zip(*curr_face[::-1]))
-    cube[face] = list(zip(*cube[face][::-1]))
-
-    # Update adjacent faces
-    update_adjacent_faces(cube)
-
-
-def update_adjacent_faces(cube):
-      # Store the first column of the right face
-      right_first_column = [cube['R'][i][0] for i in range(3)]
-
-      # Store the first row of the down face
-      down_first_row = cube['D'][0]
-
-      # Update right face from front face
-      last_row_of_front = cube['F'][2] 
-
-      # Update right face's first column with the last row of front face
-      for i in range(3):
-            cube['R'][i][0] = last_row_of_front[i]
-
-      # Update the first row of down face with the first column of right face
-      cube['D'][0] = right_first_column
-
-      # Update left face's last column with the first row of the down face
-      for i in range(3):
-            cube['L'][i][2] = down_first_row[i]
-
-def rotate_face_counter_clockwise(face, cube):
-    # rotate array counter clockwise
-    cube[face] = list(reversed(list(zip(*cube[face]))))
-
-    # Update adjacent faces
-    update_adjacent_faces_counterclockwise(cube)
-
-def update_adjacent_faces_counterclockwise(cube):
-      # Store the original first column of the left face
-      left_last_column = [cube['L'][i][2] for i in range(3)]
-
-      # Store the original first row of the down face
-      down_first_row = cube['D'][0]
-
-      # Update left face from front face
-      last_row_of_front = cube['F'][2] 
-
-      # Update left face's last column with the first row of the front face
-      for i in range(3):
-            cube['L'][i][2] = last_row_of_front[i]
-
-      # Update the first row of down face with the first column of the left face
-      cube['D'][0] = left_last_column
-
-      # Update right face's first column with the first row of the down face
-      for i in range(3):
-            cube['R'][i][0] = down_first_row[i]
-
-def rotate_top_clockwise(cube):
-      # Store original top row of left face
-      left_orig_top_row = cube['L'][0]
-
-      # Store original top row of right face
-      right_orig_top_row = cube['R'][0]
-
-      # Update top row of left face with top row of front face
-      cube['L'][0] = cube['F'][0]
-
-      # Update top row of right face with top row of back face
-      cube['R'][0] = cube['B'][0]
-
-      # Update top row of front face with original right top row
-      cube['F'][0] = right_orig_top_row
-
-      # Update top row of back face with original left top row
-      cube['B'][0] = left_orig_top_row
-
-      # Update top face by rotating array 
-      cube['T'] = list(zip(*cube['T'][::-1]))
-
-def rotate_top_counter_clockwise(cube):
-      # Store original top row of left face
-      left_orig_top_row = cube['L'][0]
-
-      # Store original top row of right face
-      right_orig_top_row = cube['R'][0]
-
-      # Update top row of left face with top row of back face
-      cube['L'][0] = cube['B'][0]
-
-      # Update top row of right face with top row of front face
-      cube['R'][0] = cube['F'][0]
-
-      # Update top row of front face with original left top row
-      cube['F'][0] = left_orig_top_row
-
-      # Update top row of back face with original right top row
-      cube['B'][0] = right_orig_top_row
-
-      # Update top face by rotating array
-      cube['T'] = list(reversed(list(zip(*cube['T']))))
-
-def rotate_bottom_clockwise(cube):
-      # Store original bottom row of left face
-      left_orig_bottom_row = cube['L'][2]
-
-      # Store original bottom row of right face
-      right_orig_bottom_row = cube['R'][2]
-
-      # Update bottom row of left face with bottom row of front face
-      cube['L'][2] = cube['F'][2]
-
-      # Update bottom row of right face with bottom row of back face
-      cube['R'][2] = cube['B'][2]
-
-      # Update bottom row of front face with original right bottom row
-      cube['F'][2] = right_orig_bottom_row
-
-      # Update bottom row of back face with original left bottom row
-      cube['B'][2] = left_orig_bottom_row
-
-      # Update bottom face by rotating array 
-      cube['D'] = list(zip(*cube['D'][::-1]))
-
-def rotate_bottom_counter_clockwise(cube):
-      # Store original bottom row of left face
-      left_orig_bottom_row = cube['L'][2]
-
-      # Store original bottom row of right face
-      right_orig_bottom_row = cube['R'][2]
-
-      # Update bottom row of left face with bottom row of back face
-      cube['L'][2] = cube['B'][2]
-
-      # Update bottom row of right face with bottom row of front face
-      cube['R'][2] = cube['F'][2]
-
-      # Update bottom row of front face with original left bottom row
-      cube['F'][2] = left_orig_bottom_row
-
-      # Update bottom row of back face with original right bottom row
-      cube['B'][2] = right_orig_bottom_row
-
-      # Update bottom face by rotating array
-      cube['D'] = list(reversed(list(zip(*cube['D']))))
-
-def rotate_right_column_clockwise(cube):
-      # Store original right column of top face
-      top_orig_right_column = [cube['T'][i][2] for i in range(3)]
-
-      # Store original right column of front face
-      front_orig_right_column = [cube['F'][i][2] for i in range(3)]
-
-      # Store original right column of bottom face
-      bottom_orig_right_column = [cube['D'][i][2] for i in range(3)]
-
-      # Store original right column of back face
-      back_orig_right_column = [cube['B'][i][0] for i in range(3)]
-
-      # Update right column of top face with right column of front face
-      for i in range(3):
-            cube['T'][i][2] = front_orig_right_column[i]
-
-      # Update right column of front face with right column of bottom face
-      for i in range(3):
-            cube['F'][i][2] = bottom_orig_right_column[i]
-
-      # Update right column of bottom face with right column of back face
-      for i in range(3):
-            cube['D'][i][2] = back_orig_right_column[i]
-
-      # Update right column of back face with right column of top face
-      for i in range(3):
-            cube['B'][i][2] = top_orig_right_column[i]
-
-      # Update right face by rotating array
-      cube['R'] = list(zip(*cube['R'][::-1]))
-
-def rotate_right_column_counter_clockwise(cube):
-      # Store original right column of top face
-      top_orig_right_column = [cube['T'][i][2] for i in range(3)]
-
-      # Store original right column of front face
-      front_orig_right_column = [cube['F'][i][2] for i in range(3)]
-
-      # Store original right column of bottom face
-      bottom_orig_right_column = [cube['D'][i][2] for i in range(3)]
-
-      # Store original right column of back face
-      back_orig_right_column = [cube['B'][i][0] for i in range(3)]
-
-      # Update right column of top face with right column of back face
-      for i in range(3):
-            cube['T'][i][2] = back_orig_right_column[i]
-
-      # Update right column of back face with right column of bottom face
-      for i in range(3):
-            cube['B'][i][2] = bottom_orig_right_column[i]
-
-      # Update right column of bottom face with right column of front face
-      for i in range(3):
-            cube['D'][i][2] = front_orig_right_column[i]
-
-      # Update right column of front face with right column of top face
-      for i in range(3):
-            cube['F'][i][2] = top_orig_right_column[i]
-
-      # Update right face by rotating array
-      cube['R'] = list(reversed(list(zip(*cube['R']))))
-
-def rotate_left_column_clockwise(cube):
-      # Store original left column of top face
-      top_orig_left_column = [cube['T'][i][0] for i in range(3)]
-
-      # Store original left column of front face
-      front_orig_left_column = [cube['F'][i][0] for i in range(3)]
-
-      # Store original left column of bottom face
-      bottom_orig_left_column = [cube['D'][i][0] for i in range(3)]
-
-      # Store original left column of back face
-      back_orig_left_column = [cube['B'][i][2] for i in range(3)]
-
-      # Update left column of top face with left column of back face
-      for i in range(3):
-            cube['T'][i][0] = back_orig_left_column[i]
-
-      # Update left column of back face with left column of bottom face
-      for i in range(3):
-            cube['B'][i][0] = bottom_orig_left_column[i]
-
-      # Update left column of bottom face with left column of front face
-      for i in range(3):
-            cube['D'][i][0] = front_orig_left_column[i]
-
-      # Update left column of front face with left column of top face
-      for i in range(3):
-            cube['F'][i][0] = top_orig_left_column[i]
-
-      # Update left face by rotating array
-      cube['L'] = list(zip(*cube['L'][::-1]))
-
-def rotate_left_column_counter_clockwise(cube):
-      # Store original left column of top face
-      top_orig_left_column = [cube['T'][i][0] for i in range(3)]
-
-      # Store original left column of front face
-      front_orig_left_column = [cube['F'][i][0] for i in range(3)]
-
-      # Store original left column of bottom face
-      bottom_orig_left_column = [cube['D'][i][0] for i in range(3)]
-
-      # Store original left column of back face
-      back_orig_left_column = [cube['B'][i][2] for i in range(3)]
-
-      # Update left column of top face with left column of front face
-      for i in range(3):
-            cube['T'][i][0] = front_orig_left_column[i]
-
-      # Update left column of front face with left column of bottom face
-      for i in range(3):
-            cube['F'][i][0] = bottom_orig_left_column[i]
-
-      # Update left column of bottom face with left column of back face
-      for i in range(3):
-            cube['D'][i][0] = back_orig_left_column[i]
-
-      # Update left column of back face with left column of top face
-      for i in range(3):
-            cube['B'][i][2] = top_orig_left_column[i]
-
-      # Update left face by rotating array
-      cube['L'] = list(reversed(list(zip(*cube['L']))))
+import video_capture as vc
+import checker as check
+import permutations as perm
+
+# current_state = vc.get_cube()
+
+test_state = {
+            'F': [['O', 'B', 'R'], ['R', 'G', 'R'], ['R', 'G', 'G']],
+            'B': [['Y', 'W', 'B'], ['W', 'B', 'W'], ['W', 'Y', 'W']],
+            'L': [['R', 'Y', 'G'], ['G', 'O', 'B'], ['G', 'Y', 'B']],
+            'T': [['Y', 'O', 'O'], ['R', 'W', 'W'], ['Y', 'O', 'Y']],
+            'D': [['W', 'O', 'R'], ['G', 'Y', 'Y'], ['O', 'B', 'O']]
+        }
+
+
+# Main function to solve white cross
+def solve_white_cross(cube):
+    while True:
+        white_edges = find_all_white_edges(cube)
+        if not white_edges:
+            break
+        for edge in white_edges:
+            face, i, j = edge
+            if face == 'D':
+                align_bottom_white_edges(cube)
+            elif face == 'T':
+                align_top_white_edge(cube, i, j)
+            else:
+                align_side_white_edge(cube, face, i, j)
+
+# Locate white edges
+def find_all_white_edges(cube):
+    white_edges = []
+    for face, grid in cube.items():
+        for row_index, row in enumerate(grid):
+            for col_index, color in enumerate(row):
+                if color == 'W':
+                    white_edges.append((face, row_index, col_index))
+    return white_edges
+
+def get_adjacent_face_and_sticker(cube, face, i, j):
+    edge_map = {
+        # Front face edges
+        ('F', 0, 1): ('T', 2, 1),  # Front top edge -> Top bottom edge
+        ('F', 1, 0): ('L', 1, 2),  # Front left edge -> Left right edge
+        ('F', 1, 2): ('R', 1, 0),  # Front right edge -> Right left edge
+        ('F', 2, 1): ('D', 0, 1),  # Front bottom edge -> Down top edge
+        
+        # Back face edges
+        ('B', 0, 1): ('T', 0, 1),  # Back top edge -> Top top edge
+        ('B', 1, 0): ('R', 1, 2),  # Back left edge -> Right right edge
+        ('B', 1, 2): ('L', 1, 0),  # Back right edge -> Left left edge
+        ('B', 2, 1): ('D', 2, 1),  # Back bottom edge -> Down bottom edge
+        
+        # Left face edges
+        ('L', 0, 1): ('T', 1, 0),  # Left top edge -> Top left edge
+        ('L', 1, 0): ('B', 1, 2),  # Left left edge -> Back right edge
+        ('L', 1, 2): ('F', 1, 0),  # Left right edge -> Front left edge
+        ('L', 2, 1): ('D', 1, 0),  # Left bottom edge -> Down left edge
+        
+        # Right face edges
+        ('R', 0, 1): ('T', 1, 2),  # Right top edge -> Top right edge
+        ('R', 1, 0): ('F', 1, 2),  # Right left edge -> Front right edge
+        ('R', 1, 2): ('B', 1, 0),  # Right right edge -> Back left edge
+        ('R', 2, 1): ('D', 1, 2),  # Right bottom edge -> Down right edge
+        
+        # Top face edges
+        ('T', 0, 1): ('B', 0, 1),  # Top top edge -> Back top edge
+        ('T', 1, 0): ('L', 0, 1),  # Top left edge -> Left top edge
+        ('T', 1, 2): ('R', 0, 1),  # Top right edge -> Right top edge
+        ('T', 2, 1): ('F', 0, 1),  # Top bottom edge -> Front top edge
+        
+        # Down face edges
+        ('D', 0, 1): ('F', 2, 1),  # Down top edge -> Front bottom edge
+        ('D', 1, 0): ('L', 2, 1),  # Down left edge -> Left bottom edge
+        ('D', 1, 2): ('R', 2, 1),  # Down right edge -> Right bottom edge
+        ('D', 2, 1): ('B', 2, 1),  # Down bottom edge -> Back bottom edge   
+    }
+    # Get adjacent position
+    adj_face, adj_i, adj_j = edge_map.get((face, i, j))
+    # Return adjacent face and its color at that position
+    return adj_face, cube[adj_face][adj_i][adj_j]
+    # returns 'L', cube['L'][2][1]
+
+def align_bottom_white_edges(cube):
+    while True:
+        white_edges = find_white_edge_on_face(cube, 'D')
+        if not white_edges:
+            break
+        i,j = white_edges[0]
+        adj_face, adj_sticker = get_adjacent_face_and_sticker(cube, 'D', i, j)  
+        if is_edge_aligned_with_center(cube, adj_face, adj_sticker):
+            #rotate specific face clockwise
+            perm.rotate_face_clockwise(cube, adj_face)
+            perm.rotate_face_clockwise(cube, adj_face)
+        else:
+            perm.rotate_bottom_clockwise(cube)
+    
+# Find the next white edge that needs solving 
+def find_next_unaligned_white_edge(cube):
+    # Check all faces except top first
+    for face in ['F', 'B', 'L', 'R', 'D']:
+        # Edge pieces are always in these positions on a face:
+        for i, j in [(1,0), (1,2), (0,1), (2,1)]:  # middle-left, middle-right, top-middle, bottom-middle
+            if cube[face][i][j] == 'W':
+                return (face, i, j)
+    
+    # If no edges found on other faces, check top face for misplaced edges
+    for j in [1, 0, 2]:  # Check positions on top face
+        if cube['T'][2][j] == 'W' and not is_edge_aligned_with_center(cube, 2, j):
+            return ('T', 2, j)
+    return None
+
+def find_white_edge_on_face(cube, face):
+    found = []
+    for i, j in [(1,0), (1,2), (0,1), (2,1)]:  # middle-left, middle-right, top-middle, bottom-middle
+        if cube[face][i][j] == 'W':
+            found.append((i, j))
+    return found
+
+def is_edge_aligned_with_center(cube, adj_face, adj_sticker):
+    return adj_sticker == cube[adj_face][1][1]
+
+def align_top_white_edge(cube, i, j):
+    attempts = 0
+    while attempts < 4:
+        adj_face, adj_sticker = get_adjacent_face_and_sticker(cube, 'T', i, j)
+        if is_edge_aligned_with_center(cube, adj_face, adj_sticker):
+            perm.rotate_face_clockwise(cube, adj_face)
+            return
+        else:
+            perm.rotate_top_clockwise(cube)
+            attempts += 1
+
+def align_side_white_edge(cube, face, i, j):
+    adj_face, adj_sticker = get_adjacent_face_and_sticker(cube, face, i, j)
+    attempts = 0
+    while not is_edge_aligned_with_center(cube, adj_face, adj_sticker) and attempts < 4: 
+        perm.rotate_face_clockwise(cube, face)
+        attempts += 1
+    if is_edge_aligned_with_center(cube, adj_face, adj_sticker):
+        perm.rotate_face_clockwise(cube, adj_face)
