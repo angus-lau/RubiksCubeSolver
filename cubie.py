c2b6f74b503f21189fea9bbf0c1a64cd18e51122
Angus <angus@angus.local>
2025-03-01 10:46:27 -0800

    finish phase 1 coordinates

--- cubie.py
+++ cubie.py
@@ -2,8 +2,20 @@ from functools import reduce
 from pieces import Corner, Edge
 from . import facecube
 
+def choose(n, k):
+    if 0 <= k <= n:
+        num = 1
+        den = 1
+        for i in range(1, min(k, n - k) + 1):
+            num *= n
+            den *= i
+            n -= 1
+        return num // den
+    else:
+        return 0
 
-# Table that shows position of cubies after an U rotation and so forth
+
+# Table that shows position of cubies after a U rotation and so forth
 _cpU = (
     Corner.UBR,
     Corner.URF,
@@ -250,6 +262,119 @@ class Cubie:
             cube.edge_ori[i] = self.edge_ori[cube.edge_ori[i]]
         
         return cube
+    
+    # Convert cubie to faceCube ... useful for vis generation as well
+    # Self being scrambled cube state
+    def to_facecube(self):
+        face = facecube.FaceCube()
+        # Return location and orientation of each corner
+        for i in range(8):
+            location = self.corner_pos[i]
+            ori = self.corner_ori[i]
+            for o in range(3):
+                face.f[facecube.corner_facelet[i][(o+ori) % 3]] = facecube.corner_color[location][o]
+        # Return location and orientation of each corner 
+        for x in range(12):
+            e = self.edge_pos[x]
+            ori = self.edge_ori[x]
+            for o in range(2):
+                facelet = facecube.edge_facelet[x][(o+ori) % 2]
+                face.f[facelet] = facecube.edge_color[e][o]
+        
+        return face
+    
+    # Check how many corners have been swapped. When compared to edge check, determine if solvable or not. 
+    def corner_check(self):
+        swaps = 0
+        for i in range(7,0,-1):
+            for j in range(i-1, -1, -1):
+                if self.cp[j] > self.cp[i]:
+                    s +=1
+        return s % 2
+    
+    # Check how many edges have been swapped. When compared to corner check, determine if solvable or not.  
+    def edge_check(self):
+        swaps = 0
+        for i in range (11, 0, -1):
+            for j in range (i - 1, -1, -1):
+                if self.ep[j] > self.ej[i]:
+                    s+=1
+        return s % 2
+    
+    # Create ternary value to represent corner orientations. 
+    def get_twist(self):
+        twist_value = 0
+        for i in range(7):
+            twist_value = 3 * twist_value + self.co[i]
+        return twist_value
+    
+    # Setter for twist, reverses ternary value that get_twist returns in order to set twist values for the scrambled cube
+    def set_twist(self, twist):
+        if not 0 <= twist < 3 ** 7:
+            raise ValueError(f'{twist} is out of range for twist, has to between 0 and 2186.')
+        self.co[:7] = [twist // (3 ** i) % 3 for i in reversed(range(7))]
+        total = sum(self.co[:7])
+        self.co[7] = (-total) % 3
+
+    # Create binary value to represent edge orientations.
+    def get_flip(self):
+        flip_value = 0
+        for i in range(11):
+            flip_value = 2 * flip_value + self.edge_ori[i]
+        return flip_value
+
+    # Setter for flip, reverses binary value that get_flip return in order to set flip values for the scrambled cube
+    def set_flip(self, flip):
+        if not 0 <= 2 ** 11:
+            raise ValueError(f'{flip} is out of range for flip, must be between 0 and 2047.')
+        self.edge_ori[:11] = [flip // (2 ** i) % 2 for i in reversed(range(11))]
+        total = sum(self.edge_ori[:11])
+        self.edge_ori[11] = (-total) % 2
+
+    # Determine if FR, FL, BL, and BR are in the middle layer. 
+    # If all are, return 0, else return non-zero number, higher values means more out of placed vals
+    def get_udslice(self):
+        udslice, seen = 0, 0
+        for j, edge in enumerate(self.edge_pos):
+            if 8 <= edge < 12:
+                seen += 1
+            elif seen > 0:
+                # Check how many ways the previously seen middle layer edges can be placed incorrectly.
+                # j: total positions checked so far in self.edge_pos 0...11
+                udslice += choose(j, seen - 1)
+        return udslice
+
+    # Determine which positions the middle layer edgs should go based on udslice val.
+    def set_udslice(self, udslice):
+        udslice_edge = [Edge.FR, Edge.FL, Edge.BL, Edge.BR]
+        other_edge = [Edge.UR, Edge.UF, Edge.UL, Edge.UB, Edge.DR, Edge.DF, Edge.DL, Edge.DB]
+        # Set edges to DB, easier to organize
+        self.edge_pos = [Edge.DB] * 12
+        # Start placing middle layer edges based on udslice 
+        seen = 3
+        for j in reversed(range(12)):
+            comb = choose(j, seen)
+            # Udslice represents value of how far edges are from correct spots, and gets reduced as we place edges.
+            # if True, place edge at ep[j], if False, we can skip placing an edge at this spot. 
+            # Udslice represents how many ways per last func, comb is how much each one costs. 
+            if udslice < comb:
+                self.edge_pos[j] = udslice_edge[seen]
+                seen -= 1
+            else:
+                udslice -= comb
+        # Fill remaining edges 
+        remaining_edges = iter(other_edge)
+        self.edge_pos = [next(remaining_edges) if edge == Edge.DB else edge for edge in self.edge_pos]
+
+
+
+
+
+
+        
+
+
+        
 
 
 
