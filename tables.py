3583a8aaaf4350bb495ed76f5f0977b46503045c
angus-lau <anguslau91@gmail.com>
2025-03-15 15:43:31 -0700

    add comments

--- tables.py
+++ tables.py
@@ -90,12 +90,12 @@ class Tables:
         for i in range(self.TWIST):
             a.twist = i
             # Loop over MOVE_CUBE list, where each move is a predefined face turn. Enumerate gives index of move, move is the actual move used 
-            for j, move in enumerate(MOVE_CUBE):
+            for face_index, move in enumerate(MOVE_CUBE):
                 # Each face move can be applied up to 3 times (quarter-turns) k = 0 -> apply move once (90), k=1 -> apply move again (180), k=2  -> apply move a 3rd time (270)
-                for k in range(3):
+                for rotation_count in range(3):
                     a.corner_transformation(move)
                     # Store the resulting twist state in the table
-                    tm[i][3 * j + k] = a.twist
+                    tm[i][3 * face_index + rotation_count] = a.twist
                 # Reset move after 3 times. 
                 a.corner_transformation(move)
         return tm
@@ -108,13 +108,13 @@ class Tables:
         for i in range(self.FLIP):
             a.flip = i
             # Loop over 6 possible face moves
-            for j in range(6):
+            for face_index in range(6):
                 # Apply the same face move 3 times over, and store each new flip state
-                for k in range(3):
-                    a.edge_transformation(MOVE_CUBE[j])
-                    ft[i][3 * j + k] = a.flip
+                for rotation_count in range(3):
+                    a.edge_transformation(MOVE_CUBE[face_index])
+                    ft[i][3 * face_index + rotation_count] = a.flip
                 # Reset flips after 3 times
-                a.edge_transformation(MOVE_CUBE[j])
+                a.edge_transformation(MOVE_CUBE[face_index])
         return ft
     
     @classmethod
@@ -138,11 +138,12 @@ class Tables:
         for i in range(self.EDGE4):
             # setting i so that we can track how it changes when moves are applied
             a.edge4 = i
-            for j in range(6):
-                for k in range(3):
+            for face_index in range(6):
+                for rotation_count in range(3):
                     a.edge_transformation(MOVE_CUBE[j])
-                    e4[i][3 * j + k] = -1 if (k % 2 == 0 and j % 3 != 0) else a.edge4
-                a.edge_transformation(MOVE_CUBE[j])
+                    e4[i][3 * face_index + rotation_count] = -1 if (rotation_count % 2 == 0 
+                                                                    and face_index % 3 != 0) else a.edge4
+                a.edge_transformation(MOVE_CUBE[face_index])
         return e4
     
     @classmethod
@@ -152,11 +153,18 @@ class Tables:
 
         for i in range(self.EDGE8):
             a.edge8 = i 
-            for j in range(6):
-                for k in range(3):
-                    a.edge_transformation(MOVE_CUBE[j])
-                    e8[i][3 * j + k] = -1 if (k % 2 == 0 and j % 3 != 0) else a.edge8
-                a.edge_transformation(MOVE_CUBE[j])
+            for face_index in range(6):
+                for rotation_count in range(3):
+                    a.edge_transformation(MOVE_CUBE[face_index])
+                    # Determine what value to store in the lookup table.
+                    # If the move is applied an even # of times (1st or 3rd move)
+                    # AND the face index is not a multiple of 3 (excluding U and F moves),
+                    # store -1 to show invalid state transition
+                    # otherwise, store the updated e4 state in lookup table
+                    e8[i][3 * face_index + rotation_count] = -1 if (rotation_count % 2 == 0
+                                                                    and face_index % 3 != 0) else a.edge8
+                # apply move one more time to return cube back to original state
+                a.edge_transformation(MOVE_CUBE[face_index])
         return e8
 
     @classmethod
@@ -165,31 +173,45 @@ class Tables:
         a = Cubie()
         for i in range(self.CORNER):
             a.corner = i
-            for j in range(6):
-                for k in range(3):
-                    a.corner_transformation(MOVE_CUBE[j])
-                    cm[i][3 * j + k] = -1 if (k % 2 == 0 and j % 3 != 0) else a.corner
-                a.corner_transformation(MOVE_CUBE[j])
+            for face_index in range(6):
+                for rotation_count in range(3):
+                    a.corner_transformation(MOVE_CUBE[face_index])
+                    cm[i][3 * face_index + rotation_count] = -1 if (rotation_count % 2 == 0 
+                                                                    and face_index % 3 != 0) else a.corner
+                a.corner_transformation(MOVE_CUBE[face_index])
         return cm
 
     @classmethod
     def make_ust_prune(self):
+        # initialize pruning table with -1 to indicate unvisited states.
         ust_prune = [-1] * (self.UDSLICE * self.TWIST)
+        # set the solved state's pruning value to 0 at index 0 indiciating it requires 0 moves
         ust_prune[0] = [0]
+        # initialize counters, count being visited, depth showing current BFS level.
+        # count starts at 1 because we already marked the solved state
+        # depth starts at 0 because the solved state is at depth 0
         count, depth = 1, 0
+        # keep running until all states are visited
         while count < self.UDSLICE * self.TWIST:
-            for i in range(self.UDSLICE * self.TWIST):
-                if ust_prune[i] == depth:
-                    n = [
-                        self.usm[i // self.TWIST][j] * self.TWIST
-                        + self.tm[i % self.TWIST][j]
+            # iterate over all possible usdlice-twist states
+            for current_state in range(self.UDSLICE * self.TWIST):
+                # if the state has the current depth, explore next possible moves
+                if ust_prune[current_state] == depth:
+                    # generate all possible next state from the current states.
+                    # pull udslice and twist of state i. See where udslice and twist move when j is applied.
+                    # multiply by twist to merge new udslice and twist
+                    neighbour_states = [
+                        self.usm[current_state // self.TWIST][j] * self.TWIST
+                        + self.tm[current_state % self.TWIST][j]
                         for j in range(18)
                     ]
 
-                    for y in n:
-                        if ust_prune[y] == -1:
+                    for next_state in neighbour_states:
+                        # if the state is unvisited (-1), increase count, set its depth to +1
+                        if ust_prune[next_state] == -1:
                             count += 1
-                            ust_prune[y] = depth + 1
+                            ust_prune[next_state] = depth + 1
+            # increase depth to process next BFS layer
             depth += 1
         return Pruning(ust_prune, self.TWIST)
 
@@ -199,11 +221,11 @@ class Tables:
         usf_prune[0] = 0
         count, depth = 1, 0
         while count < self.UDSLICE * self.FLIP:
-            for i in range(self.UDSLICE * self.FLIP):
-                if usf_prune[i] == depth:
+            for current_state in range(self.UDSLICE * self.FLIP):
+                if usf_prune[current_state] == depth:
                     n = [
-                        self.usm[i // self.FLIP][j] * self.FLIP
-                        + self.fm[i % self.FLIP][j]
+                        self.usm[current_state // self.FLIP][j] * self.FLIP
+                        + self.fm[current_state % self.FLIP][j]
                         for j in range(18)
                     ]
                     for x in n:
@@ -219,11 +241,11 @@ class Tables:
         e4e8_prune[0] = 0
         count, depth = 1,0
         while count < self.EDGE4 * self.EDGE8:
-            for i in range(self.EDGE4 * self.EDGE8):
-                if e4e8_prune[i] == depth:
+            for current_state in range(self.EDGE4 * self.EDGE8):
+                if e4e8_prune[current_state] == depth:
                     n = [
-                        self.e4[i // self.EDGE8][j] * self.EDGE8
-                        + self.e8[i % self.EDGE8][j]
+                        self.e4[current_state // self.EDGE8][j] * self.EDGE8
+                        + self.e8[current_state % self.EDGE8][j]
                         for j in range(18)
                     ]
                     for x in n:
@@ -239,11 +261,11 @@ class Tables:
         e4_corner_prune[0] = 0
         count, depth = 1, 0
         while count < self.EDGE4 * self.CORNER:
-            for i in range(self.EDGE4 * self.CORNER):
-                if e4_corner_prune[i] == depth:
+            for current_state in range(self.EDGE4 * self.CORNER):
+                if e4_corner_prune[current_state] == depth:
                     n = [
-                        self.e4[i // self.CORNER][j] * self.CORNER
-                        + self.cm[i % self.CORNER][j]
+                        self.e4[current_state // self.CORNER][j] * self.CORNER
+                        + self.cm[current_state % self.CORNER][j]
                         for j in range(18)
                     ]
 
