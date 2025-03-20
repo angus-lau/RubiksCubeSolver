ccb34375da955dd8469ae85686676fcdac8c8ebc
angus-lau <anguslau91@gmail.com>
2025-03-20 02:07:07 -0700

    import change

--- twophase/cubes/coord.py
+++ twophase/cubes/coord.py
@@ -1,5 +1,5 @@
 from .cubie import Cubie
-from ..old_tables import Tables
+from ..tables import Tables
 
 class Coord:
     def __init__(self, twist=0, flip=0, udslice=0, edge4=0, edge8=0, corner=0):
