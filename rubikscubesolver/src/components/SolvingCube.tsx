af02c87c33bd023cfed6a47981a7657b2b1bed07
angus-lau <anguslau91@gmail.com>
2025-03-29 22:12:47 -0700

    work on solvingcube

--- rubikscubesolver/src/components/SolvingCube.tsx
+++ rubikscubesolver/src/components/SolvingCube.tsx
@@ -263,7 +263,7 @@ const SolvingCube = () => {
 
   return (
     <div className="flex flex-col items-center">
-      <div ref={sketchRef} />
+      <div ref={sketchRef}/>
       <button
         onClick={() => startGlobalRotation(-1)}
         className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
